cda777b847149bf9d7d83f52169b3b4b4a626694
kacpertokaj <kacpertokajj@gmail.com>
2024-12-22 11:32:21 +0100

    nav

--- ui-app/src/components/Navbar.tsx
+++ ui-app/src/components/Navbar.tsx
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import { FaUserPlus } from "react-icons/fa";
+import { IoMdLogIn } from "react-icons/io";
+import { FaUsers } from "react-icons/fa";
 
 function Navbar() {
   return (
@@ -7,12 +10,15 @@ function Navbar() {
       <div className="links-wrapper">
         <Link className="nav-link" to="/register">
           Rejestracja
+          <FaUserPlus className="nav-icon" size={18} />
         </Link>
         <Link className="nav-link" to="/login">
           Logowanie
+          <IoMdLogIn className="nav-icon" size={18} />
         </Link>
         <Link className="nav-link" to="/users">
           Lista użytkowników
+          <FaUsers className="nav-icon" size={18} />
         </Link>
       </div>
     </nav>
--- ui-app/src/components/UserList.tsx
+++ ui-app/src/components/UserList.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect, ChangeEvent, FormEvent } from "react";
 import axios from "axios";
 import { FaTrash } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { FaArrowRight } from "react-icons/fa6";
 
 interface user {
   id: number;
@@ -16,6 +18,7 @@ export default function UserList() {
     last_name: "",
     role: "",
   });
+  const [errResponse, setErrResponse] = useState(false);
 
   useEffect(() => {
     getUsers();
@@ -30,8 +33,10 @@ export default function UserList() {
         },
       });
       setUsers(response.data);
+      setErrResponse(false);
     } catch (error) {
       console.error("Error getting users:", error);
+      setErrResponse(true);
     }
   };
 
@@ -87,89 +92,107 @@ export default function UserList() {
 
   return (
     <div className="wrapper">
-      <form className="main-form" id="userForm" onSubmit={handleAddUser}>
-        <h1 className="main-form-header">
-          Let's level up your brand, together
-        </h1>
-        <div className="inputs-wrapper">
-          <div className="input-wrapper">
-            <label htmlFor="firstName" className="form-label">
-              First name
-            </label>
-            <input
-              type="text"
-              name="first_name"
-              id="firstName"
-              placeholder="First name"
-              className="form-input"
-              value={newUser.first_name}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="input-wrapper">
-            <label htmlFor="lastName" className="form-label">
-              Last name
-            </label>
-            <input
-              type="text"
-              name="last_name"
-              id="lastName"
-              placeholder="Last name"
-              className="form-input"
-              value={newUser.last_name}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="input-wrapper">
-            <label htmlFor="role" className="form-label">
-              Role
-            </label>
-            <input
-              type="text"
-              name="role"
-              id="role"
-              placeholder="Role"
-              className="form-input"
-              value={newUser.role}
-              onChange={handleChange}
-              required
-            />
-          </div>
+      {errResponse ? (
+        <div>
+          <h1>Nie masz odpowiednich uprawnień</h1>
+          <button className="err-btn">
+            {" "}
+            <Link className="nav-link-err" to="/login">
+              Logowanie
+              <FaArrowRight className="nav-icon-err" size={18} />
+            </Link>
+          </button>
         </div>
-        <div className="agreement-wrapper">
-          <input type="checkbox" required />
-          <p className="agreement-text">
-            You agree to our friendly{" "}
-            <span className="agreement-text-underlined">privacy policy</span>
-          </p>
-        </div>
-        <button type="submit" className="submit-button">
-          Submit
-        </button>
-      </form>
-      <h1 className="main-header">Users List</h1>
-      <div className="users-wrapper">
-        {users.map((item: user, i) => {
-          return (
-            <div className="user-wrapper" key={i}>
-              <div className="user-wrapper-infos">
-                <div className="user-wrapper-infos-name">
-                  {item.first_name} {item.last_name}
-                </div>
-                <div className="user-wrapper-infos-role">{item.role}</div>
+      ) : (
+        <>
+          {" "}
+          <form className="main-form" id="userForm" onSubmit={handleAddUser}>
+            <h1 className="main-form-header">
+              Let's level up your brand, together
+            </h1>
+            <div className="inputs-wrapper">
+              <div className="input-wrapper">
+                <label htmlFor="firstName" className="form-label">
+                  First name
+                </label>
+                <input
+                  type="text"
+                  name="first_name"
+                  id="firstName"
+                  placeholder="First name"
+                  className="form-input"
+                  value={newUser.first_name}
+                  onChange={handleChange}
+                  required
+                />
               </div>
-              <button
-                className="delete-button"
-                onClick={() => deleteUser(item.id)}
-              >
-                <FaTrash size={16} />
-              </button>
+              <div className="input-wrapper">
+                <label htmlFor="lastName" className="form-label">
+                  Last name
+                </label>
+                <input
+                  type="text"
+                  name="last_name"
+                  id="lastName"
+                  placeholder="Last name"
+                  className="form-input"
+                  value={newUser.last_name}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="input-wrapper">
+                <label htmlFor="role" className="form-label">
+                  Role
+                </label>
+                <input
+                  type="text"
+                  name="role"
+                  id="role"
+                  placeholder="Role"
+                  className="form-input"
+                  value={newUser.role}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
             </div>
-          );
-        })}
-      </div>
+            <div className="agreement-wrapper">
+              <input type="checkbox" required />
+              <p className="agreement-text">
+                You agree to our friendly{" "}
+                <span className="agreement-text-underlined">
+                  privacy policy
+                </span>
+              </p>
+            </div>
+            <button type="submit" className="submit-button">
+              Submit
+            </button>
+          </form>
+          <h1 className="main-header">Users List</h1>
+          <div className="users-wrapper">
+            {users.map((item: user, i) => {
+              return (
+                <div className="user-wrapper" key={i}>
+                  <div className="user-wrapper-infos">
+                    <div className="user-wrapper-infos-name">
+                      {item.first_name} {item.last_name}
+                    </div>
+                    <div className="user-wrapper-infos-role">{item.role}</div>
+                  </div>
+                  <button
+                    className="delete-button"
+                    onClick={() => deleteUser(item.id)}
+                  >
+                    <FaTrash size={16} />
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        </>
+      )}
     </div>
   );
 }
